a7da4d5b3a076e2cf7de58852c5f5848e494bd17
Luke Frake <luke.frake@holidayextras.com>
2014-11-24 13:14:31 +0000

    starting bioSig

--- src/scripts/general.js
+++ src/scripts/general.js
@@ -1,4 +1,33 @@
-$('.stuff').on('click', '.selector', function(event) {
+var sitePrefix = "/frontlinefit"
+
+$( '.do-submitBioSig' ).on( 'click', function( event ) {
+
 	event.preventDefault();
-	/* Act on the event */
-});
+
+	console.log('sup');
+
+	$.getJSON( sitePrefix + "/js/biosig.json?" + Date.now(), function( data ) {
+		var bioSigData = data;
+		var $checkedRadios = $( 'input:checked' );
+
+		//console.log( $checkedRadios );
+
+		var loopedValues = {};
+
+		$checkedRadios.each( function() {
+			var $this = $( this );
+			loopedValues[ $this.val() ] = $this.val();
+
+			//console.log( bioSigData.content[ bioSigData.answers[ $this.val() ][ $this.attr( 'name' ) ] ] );
+
+			// If we have to have any pairs, make sure we've not had a pair previously
+			console.log( bioSigData.answers[ $this.val() ] );
+
+			//console.log( loopedValues );
+
+		} );
+
+
+	} );
+
+} );
